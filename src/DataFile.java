f58f896470784c62ab1e5f458c56f4cbbee87600
EC2 Default User <ec2-user@ip-10-88-170-86.ap-southeast-2.compute.internal>
2018-05-29 15:35:00 +0000

    clian interface

--- src/DataFile.java
+++ src/DataFile.java
@@ -7,9 +7,4 @@ public interface DataFile {
 
     public void writePage(Page p) throws IOException;
 
-    //public ArrayList<Page> insertTuple(TransactionId tid, Tuple t) throws DbException, IOException, TransactionAbortedException;
-
-    //public DbFileIterator iterator(TransactionId tid);
-
-    // public int getId();
 }
--- src/Page.java
+++ src/Page.java
@@ -5,15 +5,5 @@ public interface Page {
 
     public PageID getId();
 
-    // public TransactionId isDirty();
-
-    // public void markDirty(boolean dirty, TransactionId tid);
-
     public byte[] serialize();
-
-    // public Page getBeforeImage();
-
-    // public void setBeforeImage();
-
-    // public Iterator<Tuple> iterator();
 }
